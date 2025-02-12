47ee48280d7e37b8fe60b861bf1cb6365cde9391
German Baklagin <devprog777@gmail.com>
2025-02-12 15:45:58 +0300

    Найти два максимальных числа

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,14 @@
 package org.example;
 
 import static org.example.lessons_1.main2;
+import static org.example.lessons_1.main3;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world german!");
         System.out.println("Hello world!");
        main2();
+       main3();
     }
 
 }
--- src/main/java/org/example/lessons_1.java
+++ src/main/java/org/example/lessons_1.java
@@ -9,10 +9,54 @@ public class lessons_1 {
 
         int[] arr = {15, 27, 3, 344, 17};
         int summ = 0;
-        for(int i: arr){
+        for (int i : arr) {
             summ += i;
         }
-        System.out.println(summ /arr.length);
+        System.out.println(summ / arr.length);
+
+
+        Integer a2 = 130;
+        Integer a3 = 130;
+        System.out.println(a2 == a3);
+    }
+
+    // Найти максимальный элемент в массиве
+    public static void main3() {
+        int[] array = new int[]{64, 42, 73, 43, 65, 78, 56, 76, 32, 1, 3, 4, 54};
+        printMax(array);
+        printDoubleSummMan(array);
+    }
+
+    public static void printMax(int[] arr){
+        if(arr.length == 0){
+            System.out.println("Массив пустой");
+        }
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+
+    // Найти два максимальных числа
+    public static void printDoubleSummMan(int[] arr){
+        if (arr == null || arr.length < 2) {
+            throw new IllegalArgumentException("Массив должен содержать минимум два элемента");
+        }
+        int one = arr[0];
+        int two = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > one) {
+                two = one;
+                one = arr[i];
+            }else if (arr[i] > two) {
+                two = arr[i];
+            }
+        }
+        System.out.println(one + " " + two);
     }
 }
 
